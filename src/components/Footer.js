f24bcafb61fa9bee90ea09a29970b2fb3777af3e
kaidornel <kai_dornel@hotmail.com>
2020-07-17 16:58:03 -0400

    added links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Segment, Container, Grid, List, Header, Icon} from "semantic-ui-react"
+import {Link} from 'react-router-dom'
 
 class Footer extends React.Component {
 
@@ -12,20 +13,20 @@ class Footer extends React.Component {
                     <Grid.Row>
                         <Grid.Column width={5}>
                         <div style={{marginLeft: "120px"}}>
-                            <Icon name="github" size="huge"/>
+                            <a href="https://github.com/coDeguZo/HealthCare-Tracker-Frontend"><Icon name="github" size="huge"/></a>
                         </div>
                         </Grid.Column>
                         <Grid.Column width={5}>
                         <div style={{marginLeft: "120px"}}>
-                            <Icon name="linkedin" size="huge"/> 
-                            <Icon name="linkedin"size="huge"/>
+                            <a href="https://www.linkedin.com/in/sebastien-kai-auyong-dornel"><Icon name="linkedin" size="huge"/></a>
+                            <a href="https://www.linkedin.com/in/uzoma-ariguzo/"><Icon name="linkedin"size="huge"/></a>
                         </div> 
                         </Grid.Column>
-                        <Grid.Column width={5}>
+                        {/* <Grid.Column width={5}>
                         <div style={{marginLeft: "120px"}}>
                             <Icon name="mail" size="huge"/>
                         </div>
-                        </Grid.Column>
+                        </Grid.Column> */}
                     </Grid.Row>
                     </Grid>
                 </Container>
